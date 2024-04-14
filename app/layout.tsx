1e6822ea208b9564b77089495123018bd7889b90
Dcastillomar <125750537+Dcastillomar@users.noreply.github.com>
2024-04-14 17:16:05 -0500

    switch frame work to next.js, outline header, footer, and main page content

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter, Poppins } from "next/font/google";
+import "./globals.css";
+import Footer from "./components/footer/Footer";
+import NavBar from "./components/nav/NavBar";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
+
+export const metadata: Metadata = {
+  title: "Petsupply",
+  description: "Pet supplies Shop",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} 
+      text-slate-700`}>
+        <div className='flex flex-col min-h-screen'>
+          <NavBar />
+          <main className='flex-grow'>{children}</main>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Container from "./components/Container";
+import HomeBanner from "./components/nav/HomeBanner";
+
+export default function Home() {
+  return (
+    <div>
+      <Container>
+        <div>
+          <HomeBanner>
+            
+          </HomeBanner>
+        </div>
+      </Container>
+    </div>
+    
+  );
+}
